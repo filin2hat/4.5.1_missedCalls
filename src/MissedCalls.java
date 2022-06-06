0c7a95cb1a59c7e2e70dfee961fbb5a0411b1125
Dmitriy Biryulin <filin2hat@yandex.ru>
2022-06-06 09:03:41 +0300

    add methods to MissedCalls

--- src/MissedCalls.java
+++ src/MissedCalls.java
@@ -1,3 +1,32 @@
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MissedCalls {
+
+    private final Map<LocalDateTime, String> storage = new TreeMap<>();
+    private final PhoneBook phoneBook;
+
+    public MissedCalls(PhoneBook phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public void addMissedCalls(String phone) {
+        storage.put(LocalDateTime.now(), phone);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<LocalDateTime, String> entry : storage.entrySet()) {
+            final String phoneNumber = entry.getValue();
+            final Contact contactByPhone = phoneBook.findContactByPhone(phoneNumber);
+            sb.append(entry.getKey());
+            sb.append(" - ");
+            sb.append(contactByPhone == null ? phoneNumber : contactByPhone.getName());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
 }
